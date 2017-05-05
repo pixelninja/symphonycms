378c3dc70deadbf43440bcc894abe95421ca5b70
Alexandra Nantel <alexandra@deuxhuithuit.com>
2017-05-05 15:04:46 -0400

    Cleanup Widget::SVGIcon
    Only keep icons used in the core, the others will be added by the extensions via the registerSVGIcon method.

--- symphony/content/content.publish.php
+++ symphony/content/content.publish.php
@@ -2146,7 +2146,7 @@ class contentPublish extends AdministrationPage
         }
 
         $drawer = Widget::Drawer('section-associations', __('Show Associations'), $content);
-        $this->insertDrawer($drawer, $drawer_position, 'prepend');
+        $this->insertDrawer($drawer, $drawer_position, 'prepend', Widget::SVGIcon('associations'));
     }
 
     /**

d9a3877a9812fada7ae7ecdc76531773be3104e3
Nicolas Brassard <nitriques@users.noreply.github.com>
2019-03-22 17:12:09 -0400

    Style adjustments

--- symphony/content/content.blueprintssections.php
+++ symphony/content/content.blueprintssections.php
@@ -227,6 +227,8 @@ class contentBlueprintsSections extends AdministrationPage
 
         $formInner = new XMLElement('div', null, array('class' => 'inner'));
 
+        $this->Form->appendChild($formInner);
+
         $fieldset = new XMLElement('fieldset');
         $fieldset->setAttribute('class', 'settings');
         $fieldset->appendChild(new XMLElement('legend', __('Essentials')));
@@ -349,8 +351,6 @@ class contentBlueprintsSections extends AdministrationPage
         $fieldset->appendChild($div);
 
         $formInner->appendChild($fieldset);
-
-        $this->Form->appendChild($formInner);
 
         $this->Header->setAttribute('class', 'spaced-bottom');
         $this->Contents->setAttribute('class', 'centered-content');
@@ -475,6 +475,7 @@ class contentBlueprintsSections extends AdministrationPage
         $this->Form->appendChild($div);
 
         $formInner = new XMLElement('div', null, array('class' => 'inner'));
+        $this->Form->appendChild($formInner);
 
         $fieldset = new XMLElement('fieldset');
         $fieldset->setAttribute('class', 'settings');
@@ -597,8 +598,6 @@ class contentBlueprintsSections extends AdministrationPage
         $fieldset->appendChild($div);
 
         $formInner->appendChild($fieldset);
-
-        $this->Form->appendChild($formInner);
 
         $this->Header->setAttribute('class', 'spaced-bottom');
         $this->Contents->setAttribute('class', 'centered-content');
